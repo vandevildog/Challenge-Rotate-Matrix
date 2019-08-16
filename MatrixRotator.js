8ee4dad35c56df96300e650d3587ad89eb8b7846
phi nguyen <vandevildog@gmail.comgit config --global user.email vandevildog@gmail.comgit commit -m 1st>
2019-08-15 19:04:33 -1000

    donsko

--- MatrixRotator.js
+++ MatrixRotator.js
@@ -1,4 +1,5 @@
-const Direction = require("./Direction").Direction;
+'use strict';
+const Direction = require('./Direction').Direction;
 
 /*  MatrixRotator(matrix)
  *
@@ -11,16 +12,40 @@ const Direction = require("./Direction").Direction;
  *        @returns the rotated matrix
  */
 module.exports = class MatrixRotator {
-  constructor( matrix ) {
+  constructor(matrix) {
     this.matrix = matrix;
   }
 
   //      |-- Must be Direction.CW
   //      v        or Direction.CCW
-  rotate (direction) {
-    // do work here
+  rotate(direction) {
 
-    // must be a valid Direction, see Direction.js
+    let newArray = [];
 
+    for (let i = 0; i < this.matrix.length; i++) {
+      newArray[i] = this.matrix[i].slice();
+    }
+
+    if (direction === 'ClockWise') {
+      for (let i = 0; i < this.matrix.length; i++) {
+        for (let j = 0; j < this.matrix[0].length; j++) {
+          let changeRow = j - i;
+          let changeColumn = this.matrix.length - 1 - (j + i);
+          newArray[i + changeRow][j + changeColumn] = this.matrix[i][j];
+        }
+      }
+    } else if (direction === 'CounterClockWise') {
+      for (let i = 0; i < this.matrix.length; i++) {
+        for (let j = 0; j < this.matrix[0].length; j++) {
+          let changeColumn = i - j;
+          let changeRow = this.matrix.length - 1 - (j + i);
+          newArray[i + changeRow][j + changeColumn] = this.matrix[i][j];
+        }
+      }
+    }
+
+    this.matrix = newArray;
   }
 };
+
+    // must be a valid Direction, see Direction.js
